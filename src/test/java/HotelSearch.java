a73b2d0ee5132ad3ee3999c79463b7f0fc524631
Cynt4 <jacek.smoliniec@gmail.com>
2025-01-21 08:13:37 +0100

    Set city name

---
+++ src/test/java/HotelSearch.java
@@ -0,0 +1,22 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class HotelSearch {
+    @Test
+    public void searchHotel() {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("http://www.kurs-selenium.pl/demo/");
+        driver.findElement(By.id("s2id_autogen8")).click();
+        driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
+        driver.findElement(By.xpath("//div[@class='select2-result-label']//span")).click();
+    }
+}
+
