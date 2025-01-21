47d20c746b4f41e9cf7a6a1f8b1043aa24e73f13
Cynt4 <jacek.smoliniec@gmail.com>
2025-01-21 08:18:36 +0100

    Set check in and check out

--- src/test/java/HotelSearch.java
+++ src/test/java/HotelSearch.java
@@ -17,6 +17,11 @@ public class HotelSearch {
         driver.findElement(By.id("s2id_autogen8")).click();
         driver.findElement(By.xpath("//div[@id='select2-drop']//input")).sendKeys("Dubai");
         driver.findElement(By.xpath("//div[@class='select2-result-label']//span")).click();
+        driver.findElement(By.name("checkin")).sendKeys("17/04/2025");
+        driver.findElement(By.name("checkout")).sendKeys("30/04/2025");
+
+
+
     }
 }
 
